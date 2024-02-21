0a45b6d13bb590c46553c27860c987a59a6c7ed2
Ugur Kartal <kartalugur@gmail.com>
2024-02-21 14:36:01 +0100

    Set project

---
+++ src/main/java/net/ugurkartal/todobackend/RecapProjectSpringTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package net.ugurkartal.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProjectSpringTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapProjectSpringTodoBackendApplication.class, args);
+    }
+
+}
